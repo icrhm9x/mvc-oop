7d15277a6cf71cd2f0633e9a337fbdb60ca839f6
tungnd <tungnd@smartosc.com>
2021-04-16 20:56:19 +0700

    build directory structure and Maping router with url

--- app/Core/App.php
+++ app/Core/App.php
@@ -12,11 +12,17 @@ class App
         $this->router->get('/', function (){
             echo 'Day la trang home';
         });
+        $this->router->get('/users', function (){
+            echo 'Day la trang users';
+        });
         $this->router->post('/news', function (){
             echo 'Day la trang news';
         });
         $this->router->any('/product', function (){
             echo 'Day la trang product';
+        });
+        $this->router->any('*', function (){
+            echo '404 not found';
         });
     }
 
